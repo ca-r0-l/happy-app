92211ddfba73abf051927ea3f16d91ddf6547f82
Caroline Ribeiro <dev.caroline.ribeiro@gmail.com>
2020-12-29 00:05:25 -0300

    validate fields + cors

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import Orphanage from "../models/Orphanage";
 import orpahange_view from '../views/orphanages_view';
+import * as yup from 'yup';
 
 export default {
     async findAll(req: Request, res: Response) {
@@ -9,6 +10,7 @@ export default {
         const orphanages = await orphanagesRepository.find({
             relations: ["images"]
         });
+
         return res.json(orpahange_view.renderMany(orphanages));
     },
     async getById(req: Request, res: Response) {
@@ -39,7 +41,7 @@ export default {
             return { path: img.filename }
         });
         
-        const orphanage = orphanagesRepository.create({
+        const data = {
             name,
             latitude,
             longitude,
@@ -48,7 +50,26 @@ export default {
             opening_hours,
             open_on_weekends,
             images
+        };
+
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            latitude: yup.number().required(),
+            longitude: yup.number().required(),
+            about: yup.string().required().max(300),
+            instructions: yup.string().required(),
+            opening_hours: yup.string().required(),
+            open_on_weekends: yup.boolean().required(),
+            images: yup.array(yup.object().shape({
+                path: yup.string().required()
+            }))
         });
+
+        await schema.validate(data, {
+            abortEarly: false
+        });
+
+        const orphanage = orphanagesRepository.create(data);
     
         await orphanagesRepository.save(orphanage);
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from "path";
 import 'express-async-errors';
+import cors from 'cors';
 
 import './database/connection';
 import routes from './routes';
@@ -12,5 +13,6 @@ app.use(express.json())
 app.use(routes);
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 app.use(errorHandler);
+app.use(cors);
 
 app.listen(3333);
